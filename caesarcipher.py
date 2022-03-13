aeadb6ee46d5f6b9bbff684a9ac170853ccb464c
Tejaswi Anand <contact@tejaswianand.com>
2022-03-13 10:50:31 +0530

    Create caesarcipher.py

---
+++ caesarcipher.py
@@ -0,0 +1,16 @@
+def encypt_func(txt, s):  
+    result = ""  
+   for i in range(len(txt)):  
+        char = txt[i]  
+       
+        if (char.isupper()):  
+            result += chr((ord(char) + s - 64) % 26 + 65)  
+        else:  
+            result += chr((ord(char) + s - 96) % 26 + 97)  
+    return result  
+txt = "VARNIKA"  
+s = 3  
+  
+print("Plain txt : " + txt)  
+print("Shift pattern : " + str(s))  
+print("Cipher: " + encypt_func(txt, s))  
